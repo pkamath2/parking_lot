53064d20ed78afe0c4452c017da4e74c709ee728
pkamath2 <git.pkamath2@gmail.com>
2018-09-24 09:54:26 +0800

    Test for parking more than capacity

--- src/test/java/org/go_jek/solution/ParkingLotTest.java
+++ src/test/java/org/go_jek/solution/ParkingLotTest.java
@@ -1,6 +1,7 @@
 package org.go_jek.solution;
 
 import org.go_jek.solution.bo.Car;
+import org.go_jek.solution.execption.ParkingLotException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -30,5 +31,28 @@ public class ParkingLotTest {
 
 		assertNotEquals("Parking lot capacity should not be "+capacity, parkingLot.getSize(), capacity);
 		assertEquals("Parking lot size should be "+1, parkingLot.getSize(), 1);
+	}
+
+	@Test
+	public void shouldParkingMoreThanCapacityThrowException() {
+		int capacity = 3;
+		ParkingLot parkingLot = ParkingLot.getInstance();
+		parkingLot.createLotWithCapacity(capacity);
+		String message = null;
+		Car car1 = new Car("SHC-123", "White");
+		Car car2 = new Car("SHB-345", "Blue");
+		Car car3 = new Car("SHA-567","Red");
+		Car car4 = new Car("SHZ-789","Yellow");
+
+		parkingLot.parkCar(car1);
+		parkingLot.parkCar(car2);
+		parkingLot.parkCar(car3);
+		try {
+			parkingLot.parkCar(car4);
+		}
+		catch (ParkingLotException e) {
+			message = e.getMessage();
+		}
+		assertEquals("Parking more then capacity should throw an exception", message,"Sorry, parking lot is full");
 	}
 }
