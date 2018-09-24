5327f3d7aa1030cfc6de814813cb11d4a9b23e97
pkamath2 <git.pkamath2@gmail.com>
2018-09-24 08:56:00 +0800

    Basic Skeleton

---
+++ src/main/java/org/go_jek/solution/ParkingLotCLI.java
@@ -0,0 +1,25 @@
+package org.go_jek.solution;
+
+import java.util.Scanner;
+
+/**
+ *   Author: purnimakamath
+ */
+public class ParkingLotCLI {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		while(scanner.hasNext()){
+			String command = scanner.nextLine();
+			if(command.split(" ").length == 1) {
+				System.out.println("Consuming input from file");
+			}else{
+				System.out.println("Consuming input from command line");
+			}
+
+		}
+
+	}
+}
